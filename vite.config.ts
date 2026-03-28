aef1a21433038a516630769f92afdadc1ed3f129
davo <170838562+daftvoid@users.noreply.github.com>
2026-03-28 23:35:58 +0100

    fix(config): fixed vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
+    base: "./",
     build: {
-        rollupOptions: {
+        rolldownOptions: {
             input: {
                 index: path.resolve(__dirname, 'index.html'),
                 file: path.resolve(__dirname, 'view.html'),
