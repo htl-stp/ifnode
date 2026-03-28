088032c154ca77cd8474b0252969f917ceb19352
davo <170838562+daftvoid@users.noreply.github.com>
2026-03-28 23:24:04 +0100

    feat(view): add markdown

--- src/view.ts
+++ src/view.ts
@@ -1,3 +1,15 @@
+import { marked } from "marked";
+import hljs from "highlight.js";
+import 'highlight.js/styles/github-dark.css';
+
+function fixImagePaths(md: string) {
+    return md.replace(/!\[(.*?)]\((.*?)\)/g, (_, alt, src) => {
+        const fileName = src.split("/").pop();
+        const newSrc = `https://raw.githubusercontent.com/htl-stp/ifnode/refs/heads/main/public/data/images/${src}`;
+        return `![${alt}](${newSrc})`;
+    });
+}
+
 const params = new URLSearchParams(window.location.search);
 
 const file = params.get("file");
@@ -17,8 +29,11 @@ if (!version) {
 const url = `https://raw.githubusercontent.com/htl-stp/ifnode/refs/heads/main/public/data/history/${file}/${version}`
 
 const res = await fetch(url);
-const body = await res.text()
-
+let md = await res.text()
+md = fixImagePaths(md)
+const html = await marked(md)
 
 document.getElementById("title")!.textContent = file
-document.getElementById("content")!.textContent = body;
+document.getElementById("content")!.innerHTML = html;
+
+hljs.highlightAll()
